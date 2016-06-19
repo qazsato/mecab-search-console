a659ec822a69daa040b2b51fd3ad99f712f167ce
qazsato <qazsato@gmail.com>
2016-06-19 22:46:34 +0900

    Add coby  to clipboard button

--- public/src/scripts/result.js
+++ public/src/scripts/result.js
@@ -1,3 +1,28 @@
 $(function () {
-  $('table').tablesorter();
+  $('#result-table').tablesorter();
+  $('#copy-btn').tooltip({trigger: 'manual'});
+  $('#copy-btn').hover(function(){
+    $(this).attr('title', 'Copy to clipboard')
+      .tooltip('fixTitle')
+      .tooltip('show');
+  }, function(){
+    $(this).tooltip('hide');
+  });
+  $('#copy-btn').on('click', function () {
+    var text = document.getElementById("result-table").innerText;
+    copyClipboard(text);
+    $(this).attr('title', 'Copied!')
+      .tooltip('fixTitle')
+      .tooltip('show');
+  });
+
+  function copyClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.style.cssText = "position:absolute;left:-100%";
+    document.body.appendChild(textArea);
+    textArea.value = text;
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+  }
 });
